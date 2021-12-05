4b8a64e3fbb1546209c89f7b37608d9c0d77fab7
Kwanele Maduna <218027046@stu.ukzn.ac.za>
2021-12-05 13:31:01 +0200

    Add project files.

---
+++ GoodMatch/GoodMatchHelper/GoodMatchModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoodMatch.GoodMatchHelper
+{
+    public class GoodMatchModel
+    {
+        public string Player1Name { get; set; }
+        public string Palyer2Name { get; set; }
+        public IFormFile nextofKin { get; set; }
+    }
+}
---
+++ GoodMatch/Pages/Index.cshtml.cs
@@ -0,0 +1,81 @@
+﻿using GoodMatch.GoodMatchHelper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoodMatch.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public string Player1Name { get; set; }
+        public string Player2Name { get; set; }
+        public string singleMacthResult { get; set; }
+        public bool isThereAnError { get; set; }
+        public IOrderedEnumerable<KeyValuePair<int, List<string>>> sortedResultDictionary { get; set; }
+    public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet( bool error = true)
+        {
+            isThereAnError = error;
+
+
+        }
+        public async Task OnPost(string player1Name, string player2Name, string readFromFile)
+        {
+            try {
+                if (player1Name != null && player2Name != null && FunctionHelpers.containsOnlyAlphabets(player1Name, player2Name).Result)
+                {
+                   string charOccurances = FunctionHelpers.CountCharOccurances(player1Name,player2Name).Result;
+                  singleMacthResult =  await FunctionHelpers.writesResultsIntoFile(player1Name, player2Name, FunctionHelpers.getPercentage(charOccurances).Result);
+                   
+                    
+
+                }
+                else if (player1Name == null && player2Name == null)
+                {
+                    Dictionary<string, List<string>> groupOfPlayers = FunctionHelpers.readCSVFile().Result;
+                    for ( int i =0; i <  groupOfPlayers["Boys"].Count; i ++ ) {
+                        foreach (string girlName in groupOfPlayers["Girls"])
+                        {
+                            if (FunctionHelpers.containsOnlyAlphabets(groupOfPlayers["Boys"][i], girlName).Result)
+                            {
+                                string charOccurances = FunctionHelpers.CountCharOccurances(groupOfPlayers["Boys"][i], girlName).Result;
+                                await FunctionHelpers.writesResultsIntoFile(groupOfPlayers["Boys"][i], girlName, FunctionHelpers.getPercentage(charOccurances).Result);
+                                
+                                isThereAnError = false;
+                            }
+                            else {
+                                await FunctionHelpers.writesLogs("Error: Invalid inputs between " + groupOfPlayers["Boys"][i] + " and " + girlName);
+                                isThereAnError = true;
+                            }
+
+                        }
+                    }
+                    sortedResultDictionary = FunctionHelpers.ReadOutPutFile();
+                }
+                else {
+                    await FunctionHelpers.writesLogs("Error: Invalid inputs between "+ player1Name + " and "+ player2Name);
+                    isThereAnError = true;
+                }
+
+            } catch ( Exception ex) 
+            {
+                await FunctionHelpers.writesLogs("Error:"+ ex.Message);
+                isThereAnError = true;
+            }
+            
+            
+        
+        }
+    }
+}
